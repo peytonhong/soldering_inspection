011d29bbb0e2fc28aaf10cbd7ad9d107ef21ccea
peytonhong <hhs8314@gmail.com>
2026-06-15 13:09:52 +0900

    'first'

--- mrcnn_3d_inspection.py
+++ mrcnn_3d_inspection.py
@@ -22,7 +22,6 @@ from PIL import Image
 from torchvision.utils import draw_bounding_boxes, draw_segmentation_masks
 from tools.utils import collate_fn
 from torchvision import tv_tensors
-from convert_data import get_overlay_image
 from shutil import copyfile
 
 def get_transform(train):
@@ -132,14 +131,6 @@ def main(args):
             mask_on_image, instances = draw_mask_on_image(image_path,pred, score_threshold=0.8)
             size = mask_on_image.shape[:2]
 
-            ruled_inspection(instances,mask_on_image,size)
-
-            #ruled_inspection_length(instances, mask_on_image)
-            #ruled_inspection_no_weld(instances, mask_on_image)
-            
-           
-            #print(instances)
-
             cv2.imshow('mask_on_image',mask_on_image)
             key = cv2.waitKey()
             if key==ord('q'):
@@ -206,7 +197,6 @@ def draw_mask_on_image(image_path,pred,score_threshold=0.8):
         if label==5 :
             slit_holes.append(polygon)
 
-    beads = bead_refinement(beads, size=(h,w))
     
     #bead가 중복되는 혼란을 postprocessing = cu/al bead 병합
     instances = {'embos' : [], #{[x,y,x,y...],[x,y,x,y...]}
@@ -221,153 +211,7 @@ def draw_mask_on_image(image_path,pred,score_threshold=0.8):
 
     return image, instances
 
-def bead_refinement(beads, size):
-    mask = np.zeros(size, dtype=np.uint8)
-    for bead in beads:
-        polygon =np.array(bead).reshape((-1,2))
-        mask = cv2.fillPoly(mask, [polygon],255)
 
-        # cv2.imshow('mask',mask)
-        # cv2.waitKey()
-    
-    contours,_ = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) #마스크 윤곽선 따기 (이미지 위에 오버랩 했을 때 잘보일려고), ([ [x,y], [x,y] ...])
-
-    beads = []
-    for contour in contours:
-        polygon = contour.squeeze().flatten().tolist()
-        beads.append(polygon)
-
-    return beads
-
-def ruled_inspection(instances,image,size):
-
-    embos_lists      = instances['embos']
-    beads_lists      = instances['beads']
-    slit_holes_lists = instances['slit_holes']
-
-    for embo_list in embos_lists:
-        image, result, matched_bead,embo_xmin,embo_xmax,embo_ymin = ruled_inspection_no_weld(embo_list, beads_lists, image)
-        if result == 'NO_WELD_PASS':
-            image, result = ruled_inspection_length(matched_bead, image)
-            if result == 'BEAD_LENGTH_PASS':
-                image, result = ruled_inspection_onesided(matched_bead,embo_xmin,embo_xmax,embo_ymin,slit_holes_lists, image, size)
-
-    return image
-
-def ruled_inspection_no_weld (embo_list, beads_lists, image):
-
-    embo_array = np.array(embo_list).reshape(-1,2)
-    embo_xmin,embo_ymin = np.min(embo_array, axis=0)
-    embo_xmax,embo_ymax = np.max(embo_array, axis=0)
-
-    #bead 0개 기본 - update 방식
-    result = 'NO_WELD_FAIL'
-    color = (0,0,255)
-   
-    matched_bead = []
-    for bead_list in beads_lists:
-        bead_array = np.array(bead_list).reshape(-1,2)
-        bead_xmean, bead_ymean = np.mean(bead_array, axis=0)
-
-        if bead_xmean > embo_xmin and bead_xmean < embo_xmax :
-            result = 'NO_WELD_PASS'
-            color = (255,0,0)
-            matched_bead = bead_list
-
-    cv2.putText(image, f'{result}',(embo_xmax+10, embo_ymax+10),fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=color, thickness=1)
-
-    return image,result,matched_bead,embo_xmin,embo_xmax,embo_ymin
-
-
-def ruled_inspection_length (matched_bead,image):
-
-    bead_array = np.array(matched_bead).reshape(-1,2)
-
-    _, ymin = np.min(bead_array, axis=0)
-    xmax, ymax = np.max(bead_array, axis=0)
-
-    length_mm = (ymax - ymin) *2.5 *0.05 #resize원복(*2.5), 1픽셀당 50um 3D스캔이므로 *0.05
-
-    if length_mm >= 35:
-        result = 'BEAD_LENGTH_PASS'
-        color = (255,0,0)
-    else:
-        result = 'BEAD_LENGTH_FAIL'
-        color = (0,0,255)
-
-    cv2.putText(image, f'{result} {length_mm:.2f}mm',(xmax+10, ymax+10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=color, thickness=1)
-
-    return image,result
-
-def ruled_inspection_onesided(matched_bead, embo_xmin, embo_xmax, embo_ymin, slit_holes_lists, image, size):
-
-    slit_holes_mask = np.zeros(size, dtype=np.uint8)
-    # offset_slit_holes_mask = np.zeros(size, dtype=np.uint8)
-
-    bead_array = np.array(matched_bead).reshape(-1,2)
-    _, ymin = np.min(bead_array, axis=0)
-    _, ymax = np.max(bead_array, axis=0)
-    print('ymin',ymin)
-    print('ymax',ymax)
-    H, W = slit_holes_mask.shape[:2] #(H,W,3)에서 2개 잘라오기
-    # bead_upper_offset = np.array([[0,0],[W,0],[W,ymin+3],[0,ymin+3]]) #0.4로 resize 되었으니, 1px=50um -> 1px=125um, 4px = 0.5mm / 4px만큼 0으로 채울려면 3px 더하거나 빼줌
-    # bead_lower_offset = np.array([[0,ymax-3],[W,ymax-3],[W,H],[0,H]])
-
-    for slit_holes_list in slit_holes_lists:
-        slit = np.array(slit_holes_list).reshape(-1,2)
-        slit_xmean, _ = np.mean(slit, axis=0)
-
-        if slit_xmean > embo_xmin and slit_xmean < embo_xmax :
-            slit_holes_mask = cv2.fillPoly(slit_holes_mask, [slit], 255)
-            # offset_slit_holes_mask = cv2.fillPoly(slit_holes_mask, [bead_upper_offset, bead_lower_offset], 0)
-    
-    slit_holes_mask[ymin:ymin+4, :] = 0
-    slit_holes_mask[ymax-4:ymax, :] = 0
-    
-    contours,_ = cv2.findContours(slit_holes_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-    total_length_mm = []
-    for contour in contours:
-        polygon = contour.squeeze() #shape (polygon갯수,1,[x,y]) -> 크기가 1인 차원 제거
-        _, poly_ymin = np.min(polygon, axis=0)
-        _, poly_ymax = np.max(polygon, axis=0)
-        length_mm = (poly_ymax - poly_ymin) *2.5 *0.05
-        total_length_mm.append(length_mm)
-        print('poly_ymin', poly_ymin)
-        print('poly_ymax', poly_ymax)
-
-    if sum(total_length_mm) >= 10:
-        result = 'ONE_SIDED FAIL'
-        color = (0,0,255)
-    else :
-        result = 'ONE_SIDED PASS'
-        color = (255,0,0)
-
-    cv2.putText(image, f'{result} {sum(total_length_mm):.2f}mm',(embo_xmax+10,embo_ymin+50),fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=color, thickness=1)
-
-    return image,result
-
-def convert_tif2png(dataset_dir):
-    height_paths = glob(os.path.join(dataset_dir, "*.tif"))
-    height_paths = [height_path for height_path in height_paths if "Height" in height_path]
-
-    #png를 train과 test 폴더로 만들어서 넣기위한 makedir 코드
-    parent,child = os.path.split(dataset_dir)
-    if 'train' in child :
-        target_dir = os.path.join(parent, 'train')
-    if 'test' in child :
-        target_dir = os.path.join(parent, 'test')
-
-    os.makedirs(target_dir, exist_ok=True)
-
-    for height_path in tqdm(height_paths):
-        overlay_image = get_overlay_image(height_path)
-        overlay_image_path = os.path.basename(height_path.replace('.tif','.png'))
-        json_path = height_path.replace('.tif', '.json')
-        json_path_new = os.path.join(target_dir, os.path.basename(json_path))
-
-        cv2.imwrite(os.path.join(target_dir,overlay_image_path), overlay_image)
-        copyfile(json_path, json_path_new)
 
 def save_loss_curve(train_losses, test_losses):
     plt.plot(train_losses, label='Train Loss', marker='.')
@@ -388,7 +232,7 @@ def argparse_args():
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('command', help="'train' or 'test' or 'labeling'")
     parser.add_argument('--num_epochs', default=300, type=int, help="The number of epochs to run")
-    parser.add_argument('--batch_size', default=1, type=int, help="The number of mini-batchs for each epoch")
+    parser.add_argument('--batch_size', default=4, type=int, help="The number of mini-batchs for each epoch")
     return parser.parse_args()
 
 if __name__ == '__main__':
